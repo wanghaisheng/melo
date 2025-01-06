a1324d35eb102845bf856c6e214785f6a2543ea3
saphalpdyl <saphalpdyl@gmail.com>
2025-01-05 23:55:41 -0500

    change: route name change from app to room

--- apps/web/src/app/room/[roomId]/page.tsx
+++ apps/web/src/app/room/[roomId]/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import ChatLogs from "@/web/app/app/components/chat-logs";
-import PlayersProvider from "@/web/app/app/components/context-providers/players";
-import ConnectSocket from "@/web/app/app/components/initialization/connect-socket";
-import SocketConnection from "@/web/app/app/components/initialization/socket";
-import StreamsProvider from "@/web/app/app/components/context-providers/streams";
-import Level from "@/web/app/app/components/level";
-import VideoSection from "@/web/app/app/components/video-section";
+import ChatLogs from "@/web/app/room/components/chat-logs";
+import PlayersProvider from "@/web/app/room/components/context-providers/players";
+import ConnectSocket from "@/web/app/room/components/initialization/connect-socket";
+import SocketConnection from "@/web/app/room/components/initialization/socket";
+import StreamsProvider from "@/web/app/room/components/context-providers/streams";
+import Level from "@/web/app/room/components/level";
+import VideoSection from "@/web/app/room/components/video-section";
 import { useParams } from "next/navigation";
 
 export default function Page() {
