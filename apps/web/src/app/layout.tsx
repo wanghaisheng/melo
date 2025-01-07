ae221ce903fd154f4eb03a4b885fe3b2c99583a5
saphalpdyl <saphalpdyl@gmail.com>
2025-01-06 23:57:15 -0500

    add: implement AuthProvider and update global styles for better integration

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+
 import "./globals.css";
-import "@melo/ui/globals.css";
+// import "@melo/ui/globals.css";
+import "@melo/ui/styles/globals.css";
+
+import { AuthProvider } from "@/web/providers/auth-provider";
 
 const poppins = Poppins({
   subsets: ["latin"],
