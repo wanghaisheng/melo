063cc57de7689e16c8a3242121ef800bc00783f9
saphalpdyl <saphalpdyl@gmail.com>
2025-01-07 00:00:16 -0500

    add: integrate Firebase for authentication and Firestore setup

---
+++ apps/web/src/firebase/init.ts
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+import { FIREBASE_API_KEY, FIREBASE_APP_ID, FIREBASE_AUTH_DOMAIN, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET } from "@/web/env";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const fireauth = getAuth(app);
+export const firestore = getFirestore(app);
---
+++ apps/web/src/middleware.ts
@@ -0,0 +1,25 @@
+import { fireauth } from '@/web/firebase/init';
+import { signInAnonymously, signOut } from 'firebase/auth';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     * - auth/ ( authentication routes )
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|auth).*)',
+  ],
+}
+
+export function middleware(request: NextRequest) {
+  const user = fireauth.currentUser;
+
+  // if (!user) return NextResponse.redirect(new URL("/auth/sign-in/", request.url));
+  
+  return NextResponse.next();
+}
